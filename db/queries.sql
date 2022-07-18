c5d29cbbd683f1b08a02f6bb09a794acb259113f
MosNes <7hekarl [at] gmail.com>
2022-07-18 12:58:56 -0400

    added newRole() function

--- db/queries.sql
+++ db/queries.sql
@@ -1,4 +1,8 @@
 -- create new department
-INSERT INTO department (name)
-VALUES
-    ('Test Department');
+-- INSERT INTO department (name)
+-- VALUES
+--     ('Test Department');
+
+-- create new role
+INSERT INTO role (title, salary, department_id)
+VALUES ('Luchador', 95000, 8);
