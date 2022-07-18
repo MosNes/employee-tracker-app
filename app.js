40ddda26652d27800dd7b4bb9962828e43b9e40d
MosNes <7hekarl@gmail.com>
2022-07-18 11:44:11 -0400

    debugged and refactored all SQL functions into async functions, created initial functions for SQL queries in sqlFunctions.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 //-----DEPENDENCIES AND GLOBAL VARIABLES-----------------------------------------
 const dotenv = require('dotenv');
 dotenv.config();
-const db = require('./db/connection');
+const dbPromise = require('./db/connection');
 const Session = require('./lib/Session');
 
 //------INITIALIZATION----------------------------------------------------------------
 
+// async function test() {
+//     const db = await dbPromise;
+//     let result = await db.query(`SELECT * FROM employee WHERE id = 10`);
+//     return result;
+// }
+    
+// test().then(result => console.log(result[0]));
+
 new Session().initializeSession();
 
 
-// async function test() {
-//     (await db).query(`SELECT * FROM employee`).then(result => {
-//         console.log(result[0])
-//     });
-// }
-    
-// test();
