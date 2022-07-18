d27eb006d6febe5944d62d26d848f3b9e2597610
MosNes <7hekarl@gmail.com>
2022-07-18 18:58:59 -0400

    added getBudget() method

--- db/queries.sql
+++ db/queries.sql
@@ -25,6 +25,12 @@
 -- SELECT * FROM employee WHERE manager_id = 18;
 
 -- view employees by department
-SELECT department.name AS department_name, employee.id AS employee_id, employee.first_name, employee.last_name,  role.title FROM employee
-LEFT JOIN role ON employee.role_id = role.id
-LEFT JOIN department ON role.department_id = department.id WHERE department.id = 3;
+-- SELECT department.name AS department_name, employee.id AS employee_id, employee.first_name, employee.last_name,  role.title FROM employee
+-- LEFT JOIN role ON employee.role_id = role.id
+-- LEFT JOIN department ON role.department_id = department.id WHERE department.id = 3;
+
+-- get department budget
+SELECT department.id AS department_id, department.name AS department_name, SUM(role.salary) AS budget FROM department
+RIGHT JOIN role ON department.id = role.department_id
+RIGHT JOIN employee ON role.id = employee.role_id
+WHERE department_id = 1;
