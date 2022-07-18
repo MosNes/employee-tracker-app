b12ed38a5e3a9afa3cfad2ee5adc7ee19f8f27eb
MosNes <7hekarl [at] gmail.com>
2022-07-18 13:12:23 -0400

    added newEmployee function, refactored all sql code to return de-nested objects and arrays

--- db/queries.sql
+++ db/queries.sql
@@ -4,5 +4,9 @@
 --     ('Test Department');
 
 -- create new role
-INSERT INTO role (title, salary, department_id)
-VALUES ('Luchador', 95000, 8);
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES ('Luchador', 95000, 8);
+
+-- create new Employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Hoopsy', 'Blimdo', 17, 3);
